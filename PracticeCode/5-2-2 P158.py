aa8526af08007fbef037d0db0091da038df607ec
CELFS <634798049@qq.com>
2022-03-20 04:59:04 +0800

    Some codes in ML -- beginning

---
+++ PracticeCode/5-2-2 P158.py
@@ -0,0 +1,25 @@
+# Step 1
+import numpy as np
+import matplotlib.pyplot as plt
+
+# 读入训练数据
+train = np.loadtxt('click.csv', delimiter=',', skiprows=1)
+train_x = train[:,0]
+train_y = train[:,1]
+
+# 绘图
+plt.plot(train_x, train_y, 'o')
+plt.show()
+
+# Step 2
+# 参数初始化
+theta0 = np.random.rand()
+theta1 = np.random.rand()
+
+# 预测函数
+def f(x):
+    return theta0 + theta1 * x
+
+# 目标函数
+def E(x, y):
+    return 0.5 * np.sum((y - f(x)) ** 2)
---
+++ PracticeCode/5-2-5 P162.py
@@ -0,0 +1,79 @@
+# Step 1
+import numpy as np
+import matplotlib.pyplot as plt
+
+# 读入训练数据
+train = np.loadtxt('click.csv', delimiter=',', skiprows=1)
+train_x = train[:,0]
+train_y = train[:,1]
+
+# 绘图
+# plt.plot(train_x, train_y, 'o')
+# plt.show()
+
+# Step 2
+# 参数初始化
+theta0 = np.random.rand()
+theta1 = np.random.rand()
+
+# 预测函数
+def f(x):
+    return theta0 + theta1 * x
+
+# 目标函数
+def E(x, y):
+    return 0.5 * np.sum((y - f(x)) ** 2)
+
+# Step 3
+# 标准化
+mu = train_x.mean()
+sigma = train_x.std()
+def standardize(x):
+    return (x - mu) / sigma
+
+train_z = standardize(train_x)
+
+# Step 4
+# plt.plot(train_z, train_y, 'o')
+# plt.show()
+
+# Step 5
+# 学习率
+ETA = 1e-3
+
+# 误差的差值
+diff = 1
+
+# 更新次数
+count = 0
+
+# 重复学习
+error = E(train_z, train_y)
+while diff > 1e-2:
+    # 更新结果保存到临时变量
+    tmp0 = theta0 - ETA * np.sum(f(train_z) - train_y)
+    tmp1 = theta1 - ETA * np.sum((f(train_z) - train_y) * train_z)
+    # 更新参数
+    theta0 = tmp0
+    theta1 = tmp1
+    # 计算与上一次误差的差值
+    current_error = E(train_z, train_y)
+    diff = error - current_error
+    error = current_error
+    # 输出日志
+    count += 1
+    log = '第{}次 : theta0 = {}, theta1 = {:.3f}, 差值 = {:.4f}'
+    print(log.format(count, theta0, theta1, diff))
+
+
+
+
+
+
+
+
+
+
+
+
+
