786a548f879383433e5044ee1bdef38b5a324f15
CELFS <634798049@qq.com>
2022-04-19 21:33:00 +0800

    notes

---
+++ FunctionLibraryLearning/getattr().py
@@ -0,0 +1,9 @@
+def getattr(object, name, default=None):  # known special case of getattr
+    """
+    getattr(object, name[, default]) -> value
+
+    Get a named attribute from an object; getattr(x, 'y') is equivalent to x.y.
+    When a default argument is given, it is returned when the attribute doesn't
+    exist; without it, an exception is raised in that case.
+    """
+    pass
